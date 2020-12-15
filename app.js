1b937317ecb8e4486b8a202eba3c77139b80312e
ScyllaVessel <52348162+ScyllaVessel@users.noreply.github.com>
2020-12-15 22:01:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+    <div class="box">
+    <img src = {norris}/>
+    <h2>Chuck Norris Jokes</h2>
+    <h5>Click on any category to generate a joke from that category</h5>
+    <h4>Click on any category to generate a joke from that category</h4>
+    <h5>Pick your jokes folks!</h5>
+    <p>{joke}</p>
+
+    <button onClick={generateJoke}>Smash Tot</button>
